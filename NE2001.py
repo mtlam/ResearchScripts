ce4369a11f0ba00816b9a5d9d979469ae2bdf8f4
Michael Lam <astromlam@gmail.com>
2017-12-30 10:59:57 -0500

    Python 3 compatible

--- NE2001.py
+++ NE2001.py
@@ -5,6 +5,12 @@ import subprocess
 import os
 import sys
 
+if sys.version_info.major == 2:
+    fmap = map    
+elif sys.version_info.major == 3:
+    fmap = lambda x,*args: list(map(x,*args))
+    xrange = range
+
 
 def NE2001(l,b,DM_D,ndir=1,DIR='/home/michael/Research/Programs/NE2001/bin.NE2001/'):
     cwd = os.getcwd()
@@ -13,7 +19,7 @@ def NE2001(l,b,DM_D,ndir=1,DIR='/home/michael/Research/Programs/NE2001/bin.NE200
     proc = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
     output = [line.strip() for line in proc.stdout]
     output = output[6:]
-    values = map(lambda x: float(x.split()[0]),output)
+    values = fmap(lambda x: float(x.split()[0]),output)
     retval = dict()
     retval['input'] = dict()
     retval['input']['l'] = l
@@ -22,8 +28,7 @@ def NE2001(l,b,DM_D,ndir=1,DIR='/home/michael/Research/Programs/NE2001/bin.NE200
     retval['input']['ndir'] = ndir
     retval['output'] = dict()
     varlist = ['DIST','DM','DMz','SM','SMtau','SMtheta','SMiso','EM','TAU','SBW','SCINTIME','THETA_G','THETA_X','NU_T']
-    for i in range(len(varlist)):
-        var = varlist[i]
+    for i,var in enumerate(varlist):
         retval['output'][var] = values[i]
     os.chdir(cwd)
     return retval
@@ -31,8 +36,8 @@ def NE2001(l,b,DM_D,ndir=1,DIR='/home/michael/Research/Programs/NE2001/bin.NE200
 
 if __name__=='__main__':
     if len(sys.argv) == 1:
-        print NE2001(45,5,50,1)#Just test it out
+        print(NE2001(45,5,50,1))#Just test it out
     elif len(sys.argv) == 5:
-        args = map(lambda x: float(x),sys.argv[1:])
-        print NE2001(*args)
+        args = fmap(lambda x: float(x),sys.argv[1:])
+        print(NE2001(*args))
 
