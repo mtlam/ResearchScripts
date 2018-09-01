c9e36eb00745ce616ea87c09bd3200027af871ce
Michael Lam <astromlam@gmail.com>
2018-09-01 16:26:43 -0400

    Create psrcatNE.py

---
+++ psrcatNE.py
@@ -0,0 +1,19 @@
+import psrcat
+import os
+import sys
+import subprocess
+
+
+psr = str(sys.argv[1])
+
+d = psrcat.psrcat(psr,["Gl","Gb","DM"])
+
+
+DIR='/home/michael/Research/Programs/NE2001/bin.NE2001/'
+cwd = os.getcwd()
+os.chdir(DIR)
+cmd = './NE2001 %s %s %s %i' % (d['Gl'],d['Gb'],d['DM'],1)
+proc = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+for line in proc.stdout:
+    print line[:-1]
+os.chdir(cwd) #not necessary
