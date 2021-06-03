eadb24fd96f268d2e714f7798d7af2adcbec9124
Michael Lam <astromlam@gmail.com>
2021-06-02 21:43:04 -0400

    Encoding = ASCII to avoid byte issues

--- psrcatNE.py
+++ psrcatNE.py
@@ -13,7 +13,7 @@ DIR='/home/michael/Research/Programs/NE2001/bin.NE2001/'
 cwd = os.getcwd()
 os.chdir(DIR)
 cmd = './NE2001 %s %s %s %i' % (d['Gl'],d['Gb'],d['DM'],1)
-proc = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+proc = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,encoding="ascii")
 for line in proc.stdout:
     print line[:-1]
 os.chdir(cwd) #not necessary
